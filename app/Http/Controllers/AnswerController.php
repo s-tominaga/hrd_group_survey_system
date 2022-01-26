3fc68657ed10ca66810f3ed4d5cf63b25f9e26d5
Shigeru Tominaga <s-tominaga@hrd-s.com>
2022-01-26 12:48:19 +0800

    Created Answer.php Model class with migration script & the resource controller.

---
+++ app/Http/Controllers/AnswerController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Answer;
+use Illuminate\Http\Request;
+use App\Models\Answer as Feedback;
+
+class AnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Feedback::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $feedback = new Feedback();
+        $feedback->question = '今日は何時に帰りますか？';
+        $feedback->answer = json_encode(array(
+            'answer' => '16時半です。'
+        ));
+        $feedback->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Answer $answer)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_01_26_040236_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->json('answer');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
